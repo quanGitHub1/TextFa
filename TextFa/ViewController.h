8243229d2b1c44c22b29b5d681bdd27b7a8b721c
leeco <wangfaquan@le.com>
2018-01-31 15:00:38 +0800

    Initial Commit

---
+++ TextFa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextFa
+//
+//  Created by leeco on 2018/1/31.
+//  Copyright © 2018年 leeco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TextFa/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TextFa
+//
+//  Created by leeco on 2018/1/31.
+//  Copyright © 2018年 leeco. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
